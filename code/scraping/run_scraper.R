812c6890a29021a70efd30192f2fdf3b51be5e2c
Alec-lgtm <alecgchen@gmail.com>
2024-12-16 20:36:48 -0600

    0.1.8 Tested logging functionality in run_scraper.R

--- code/scraping/run_scraper.R
+++ code/scraping/run_scraper.R
@@ -6,13 +6,14 @@
 
 
 # ---- Article Scraping ----
-# Takes about 15-20 minutes to run
+# Takes about 10-15 minutes to run
+# Logs are stored in scraping_log.txt
 
 # Load libraries and functions
 source('scraping_functions.R')
 
 # Extract titles, url, text, and date for all vox articles in the year 2024
-all_vox_articles_2024_raw <- map(1:12,scrape_vox_month, debug_month = TRUE, debug_page = TRUE, debug_articles = FALSE) %>%
+all_vox_articles_2024_raw <- map(1:12,scrape_vox_month, debug_month = TRUE, debug_page = TRUE, debug_article = FALSE) %>%
   list_rbind()
 
 # Remove vox sites that are chronologies / timelines or aggregations of other articles
@@ -27,10 +28,10 @@ all_vox_articles_2024 <- all_vox_articles_2024 %>%
   filter(!str_detect(text, "Today, Explained"))
 
 # Save cleaned dataset
-saveRDS(all_vox_articles_2024, file = "../data/vox_articles/2024_all_vox_articles.rds")
-write.csv(all_vox_articles_2024, file = "../data/vox_articles/2024_all_vox_articles.csv", row.names = FALSE)
+saveRDS(all_vox_articles_2024, file = "../../data/vox_articles/2024_all_vox_articles.rds")
+write.csv(all_vox_articles_2024, file = "../../data/vox_articles/2024_all_vox_articles.csv", row.names = FALSE)
 
 # Save raw dataset
-saveRDS(all_vox_articles_2024_raw, file = "../data/vox_articles/2024_all_vox_articles_raw.rds")
-write.csv(all_vox_articles_2024_raw, file = "../data/vox_articles/2024_all_vox_articles_raw.csv", row.names = FALSE)
+saveRDS(all_vox_articles_2024_raw, file = "../../data/vox_articles/2024_all_vox_articles_raw.rds")
+write.csv(all_vox_articles_2024_raw, file = "../../data/vox_articles/2024_all_vox_articles_raw.csv", row.names = FALSE)
 
