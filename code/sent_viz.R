eea5a1c4186d79f72aeb78e8e9ea85f872716a3f
Alec-lgtm <alecgchen@gmail.com>
2024-12-17 11:48:14 -0600

    0.1.11 Halfway through documentation of sent_viz.R

--- code/sent_viz.R
+++ code/sent_viz.R
@@ -6,72 +6,63 @@ library(readtext)
 library(tidytext)
 library(stringr)
 
+
 # ---- Data Cleaning ----
 
 ### Podcasts
 podcast_file_paths <- list.files(path = "../data/vox_podcasts", full.names = T, recursive = T)
 podcast_transcripts <- readtext(podcast_file_paths)
+articles_text <- readRDS("../data/vox_articles/2024_all_vox_articles.rds")
 
-# cleans the transcript so that it only contains alphanumerics and creates a date column
+# Cleans the podcast and article text so that they only contains alphanumerics and creates a date column
 podcast_transcripts_clean <- podcast_transcripts %>%
   mutate(text = str_remove_all(text, "[^[:alpha:][:space:]]"), date = str_extract(doc_id, "[0-9]*_[0-9]*_[0-9]*"))
+articles_text_clean <- articles_text %>%
+  mutate(text = str_remove_all(text, "[^[:alpha:][:space:]]"), doc_id = title, date = datetime)
 
-# Converts the date column into a Date object
+# Converts the date column into an R Date object
 podcast_transcripts_clean$date <- as.Date(podcast_transcripts_clean$date,format = "%m_%d_%y")
+articles_text_clean$date <- format(articles_text_clean$date, "%Y-%m-%d")
+articles_text_clean$date <- as.Date(articles_text_clean$date, format = "%Y-%m-%d")
 
 # Add podcast title
 podcast_transcripts_clean$title <- str_extract(podcast_transcripts_clean$doc_id, "\\d+_\\d+_\\d+_\\s*(.*?)\\.docx")
 
-#splits the cleaned transcript to individual words so that we can run an sentiment analysis on it
+# Splits the cleaned text into individual words so that we can run an sentiment analysis on it
 podcast_transcripts_words <- podcast_transcripts_clean %>%
   unnest_tokens(word, text) %>%
-  anti_join(stop_words, by = "word")
-##1562.74 words per podcast average
-
-
-### Articles
-articles_text <- readRDS("../data/vox_articles/2024_all_vox_articles.rds")
-
-articles_text_clean <- articles_text %>%
-  mutate(text = str_remove_all(text, "[^[:alpha:][:space:]]"), doc_id = title, date = datetime)
-articles_text_clean$date <- format(articles_text_clean$date, "%Y-%m-%d")
-articles_text_clean$date <- as.Date(articles_text_clean$date, format = "%Y-%m-%d")
-
+  anti_join(stop_words, by = "word") # 1562.74 words per podcast average
 articles_text_words <- articles_text_clean %>%
   unnest_tokens(word, text) %>%
-  anti_join(stop_words, by = "word")
-
-##680.40 words per article average
+  anti_join(stop_words, by = "word") #680.40 words per article average
 
 
-# ---- Syuzhet Sentiment Analysis ----
+# ---- Sentiment Analysis ----
 
-### Podcasts
+### Syuzhet Built-in analysis
 
+# Calculates the sentiment score for each document in the podcasts and articles
 podcasts_sentiment <- get_sentiment(podcast_transcripts_clean$text, method="syuzhet") # sentiment per document
-podcasts_sentiment_mean <- mean(podcasts_sentiment)
+articles_sentiment <- get_sentiment(articles_text_clean$text, method="syuzhet") # sentiment per document
 
-# NRC: National Research Council Canada
-podcasts_nrc_data <- get_nrc_sentiment(podcast_transcripts$text) #nrc for text as a whole
+# Gets mean sentiment score
+podcasts_sentiment_mean <- mean(podcasts_sentiment) # get mean sentiment score
+articles_sentiment_mean <- mean(articles_sentiment)
 
+# Calculates sentiment score on the National Research Council Canada metrics
+podcasts_nrc_data <- get_nrc_sentiment(podcast_transcripts$text)
+articles_nrc_data <- get_nrc_sentiment(articles_text$text)
+
+# Add sentiment score to original podcast and article dataset
 podcasts_sentiment_by_date <- podcast_transcripts_clean %>%
   mutate(sentiment = podcasts_sentiment)
+articles_sentiment_by_date <- articles_text_clean %>%
+  mutate(sentiment = articles_sentiment)
 
+# Only get podcast sentiment scores from the year 2024
 podcasts_sentiment_2024 <- podcast_transcripts_clean %>%
   mutate(sentiment = podcasts_sentiment) %>%
   filter(date >= ymd("24-01-01"))
-
-
-### Articles
-
-articles_sentiment <- get_sentiment(articles_text_clean$text, method="syuzhet") # sentiment per document
-
-articles_sentiment_mean <- mean(articles_sentiment)
-
-articles_nrc_data <- get_nrc_sentiment(articles_text$text) #nrc for text as a whole
-
-articles_sentiment_by_date <- articles_text_clean %>%
-  mutate(sentiment = articles_sentiment)
 
 # ---- Individual Points ----
 
