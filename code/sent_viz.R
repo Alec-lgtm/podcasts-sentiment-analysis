4b9f0782fa340fc99993c751574a8de39fd59711
Alec-lgtm <alecgchen@gmail.com>
2024-12-16 22:31:37 -0600

    0.1.9 Converted half of sentiment_visualizations.qmd to snake_case

--- code/sent_viz.R
+++ code/sent_viz.R
@@ -6,43 +6,72 @@ library(readtext)
 library(tidytext)
 library(stringr)
 
-double_fix <- function(x) {
-    return(x * 2)
-}
+# ---- Data Cleaning ----
 
+#### Podcasts
+podcast_file_paths <- list.files(path = "../data/vox_podcasts", full.names = T, recursive = T)
+podcast_transcripts <- readtext(podcast_file_paths)
 
-Pfiles <- list.files(path = "../data/vox_podcasts", full.names = T, recursive = T)
-Ptranscript <- readtext(Pfiles)
-
-#cleans the transcript so that it only contains alphanumerics and creates a date column
-Ptranscript_cleaned <- Ptranscript %>%
+# cleans the transcript so that it only contains alphanumerics and creates a date column
+podcast_transcripts_clean <- podcast_transcripts %>%
   mutate(text = str_remove_all(text, "[^[:alpha:][:space:]]"), date = str_extract(doc_id, "[0-9]*_[0-9]*_[0-9]*"))
 
-#makes the date column a date variable
-Ptranscript_cleaned$date <- as.Date(Ptranscript_cleaned$date,format = "%m_%d_%y")
+# Converts the date column into a Date object
+podcast_transcripts_clean$date <- as.Date(podcast_transcripts_clean$date,format = "%m_%d_%y")
 
 # Add podcast title
-Ptranscript_cleaned$title <- str_extract(Ptranscript_cleaned$doc_id, "\\d+_\\d+_\\d+_\\s*(.*?)\\.docx")
+podcast_transcripts_clean$title <- str_extract(podcast_transcripts_clean$doc_id, "\\d+_\\d+_\\d+_\\s*(.*?)\\.docx")
 
 #splits the cleaned transcript to individual words so that we can run an sentiment analysis on it
-Ptranscript_split <- Ptranscript_cleaned %>%
+podcast_transcripts_words <- podcast_transcripts_clean %>%
   unnest_tokens(word, text) %>%
   anti_join(stop_words, by = "word")
-
 ##1562.74 words per podcast average
 
-#Articles cleaning
-VoxA_transcript <- readRDS("../data/vox_articles/2024_all_vox_articles.rds")
 
+#### Articles
+vox_articles_text <- readRDS("../data/vox_articles/2024_all_vox_articles.rds")
 
-VoxA_cleaned <- VoxA_transcript %>%
+vox_articles_text_clean <- vox_articles_text %>%
   mutate(text = str_remove_all(text, "[^[:alpha:][:space:]]"), doc_id = title, date = datetime)
-VoxA_cleaned$date <- format(VoxA_cleaned$date, "%Y-%m-%d")
-VoxA_cleaned$date <- as.Date(VoxA_cleaned$date, format = "%Y-%m-%d")
+vox_articles_text_clean$date <- format(vox_articles_text_clean$date, "%Y-%m-%d")
+vox_articles_text_clean$date <- as.Date(vox_articles_text_clean$date, format = "%Y-%m-%d")
 
-VoxA_split <- VoxA_cleaned %>%
+vox_articles_text_words <- vox_articles_text_clean %>%
   unnest_tokens(word, text) %>%
   anti_join(stop_words, by = "word")
 
 ##680.40 words per article average
+
+
+# ---- Syuzhet Sentiment Analysis
+
+#### Podcasts
+
+podcasts_sentiment <- get_sentiment(podcast_transcripts_clean$text, method="syuzhet") # sentiment per document
+podcasts_sentiment_mean <- mean(podcasts_sentiment)
+
+# NRC: National Research Council Canada
+podcasts_nrc_data <- get_nrc_sentiment(podcast_transcripts$text) #nrc for text as a whole
+
+podcasts_sentiment_by_date <- podcast_transcripts_clean %>%
+  mutate(sentiment = podcasts_sentiment)
+
+podcasts_sentiment_2024 <- podcast_transcripts_clean %>%
+  mutate(sentiment = podcasts_sentiment) %>%
+  filter(date >= ymd("24-01-01"))
+
+
+#### Articles
+
+articles_sentiment <- get_sentiment(vox_articles_text_clean$text, method="syuzhet") # sentiment per document
+
+articles_sentiment_mean <- mean(articles_sentiment)
+
+articles_nrc_data <- get_nrc_sentiment(vox_articles_text$text) #nrc for text as a whole
+
+articles_sentiment_by_date <- vox_articles_text_clean %>%
+  mutate(sentiment = articles_sentiment)
+
+
 
