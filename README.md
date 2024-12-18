dae2eb25e0df4462d64ed0bf01f9a256325e75eb
Alec-lgtm <alecgchen@gmail.com>
2024-12-18 15:50:45 -0600

    0.2.0 Finish Blog and READ.ME

--- README.md
+++ README.md
@@ -1,20 +1,87 @@
+# Sentiment analysis on Vox Podcasts and Articles
 
+Project by Aki Wada and Alec Chen
 
-# Setup
+---
 
-- Python packages
-- R packages
+## Table of Contents
 
-# Overivew
+- [About the Project](#about-the-project)
+- [Getting Started](#getting-started)
+- [Overview](#usage)
+- [Acknowledgement](#features)
 
-- Two files to run
-- visualizations.py
-- sentiment_visualizations.qmd
+---
 
-Fix the names of these please
+## About the Project
 
-# Acknowledgement
+This project is sentiment analysis of vox articles and podcasts using the `syzuhet` library.
 
-Bheggeseth
+### Built With
 
+- [R](https://www.r-project.org)
+- [Python](https://www.python.org/)
+
+---
+
+## Getting Started
+
+### Prerequisites
+
+Install these Python libraries:
+
+```bash
+pip install matplotlib wordcloud pandas seaborn nltk python-docx
+```
+
+Also make sure to download these stop words:
+
+```python
+import nltk
+nltk.download('stopwords')
+```
+
+Install these R libraries:
+
+```R
+install.packages(c("tidyverse", "rvest", "stringr", "logging", 
+                   "syuzhet", "tm", "ggplot2", "readtext", 
+                   "tidytext", "zoo"))
+```
+
+## Overivew
+
+**Blog**
+
+This is a standalong blog that walks you through our analysis process. To read it, open the `blog.html` file
+
+**Scraping**
+
+To run the scraping script, navigate to the `code/scraping/` directory and run the following command in your R console:
+
+```R
+source("run_scraper.R")
+```
+
+**Visualizations**
+
+There are three scripts we used to create these visualizations. They are all located in the `code/visualizations/` directory. To run the word frequency visualizations created by python scripts use the following command:
+
+```Python
+python viz_word_freq_articles.py
+```
+
+The figures from the python script will be located in the `figures/` directory.
+
+For the `syzuhet` visualizations, you can either open the `viz_sentiment.qmd` to run each code chunk in a quarto document, or navigate to the visualizations directory and run the following script in your R console:
+
+```R
+source("viz_sentiment_no_headings.R")
+```
+
+This script does the data cleaning and visualization for `syzuhet` and stores the data in an R workspace located in the `data` directory
+
+## Acknowledgement
+
+We like to thank our Professor Brianna Heggeseth for all the work she'd done to help us grow through this project. It couldn't have been done without her.
 
--- code/visualizations/viz_sentiment_no_headings.R
+++ code/visualizations/viz_sentiment_no_headings.R
@@ -136,7 +136,7 @@ plot(best_sentiment_article_value,
      ylab= "Emotional Valence")
 
 # Function to visualize podcast sentiment
-visualize_podcast_sentiment <- function(sentiment_vector, window_size = 20) {
+visualize_podcast_sentiment <- function(sentiment_vector, window_size = 20,name) {
   # Create a data frame with the raw sentiment data
   df <- data.frame(
     time = 1:length(sentiment_vector),
@@ -164,7 +164,7 @@ visualize_podcast_sentiment <- function(sentiment_vector, window_size = 20) {
               size = 1) +
     # Add theme and labels
     theme_classic() +
-    labs(title = paste("Podcast Sentiment Analysis\n", 
+    labs(title = paste(name,
                        window_size, "-word Moving Average", sep=""),
          x = "Narrative Time",
          y = "Sentiment Score") +
@@ -183,15 +183,40 @@ visualize_podcast_sentiment <- function(sentiment_vector, window_size = 20) {
 }
 
 # Plot best and worst article and podcast
-article_worst <- visualize_podcast_sentiment(worst_sentiment_article_value,  window_size = 10)
-podcast_worst <- visualize_podcast_sentiment(worst_sentiment_podcast_value, window_size = 10)
-article_best <- visualize_podcast_sentiment(best_sentiment_article_value, window_size = 10)
-podcast_best <- visualize_podcast_sentiment(best_sentiment_podcast_value, window_size = 10)
+article_worst <- visualize_podcast_sentiment(worst_sentiment_article_value,  window_size = 10, "Article Sentiment Analysis\n")
+podcast_worst <- visualize_podcast_sentiment(worst_sentiment_podcast_value, window_size = 10, "Podcast Sentiment Analysis\n")
+article_best <- visualize_podcast_sentiment(best_sentiment_article_value, window_size = 10, "Article Sentiment Analysis\n")
+podcast_best <- visualize_podcast_sentiment(best_sentiment_podcast_value, window_size = 10, "Podcast Sentiment Analysis\n")
 
 # Save plots
 ggsave("../../figures/worst_article_sentiment_rolling_average_10.png", article_worst, bg = "white", width = 10, height = 6)
 ggsave("../../figures/worst_podcast_sentiment_rolling_average_10.png", podcast_worst, bg = "white", width = 10, height = 6)
 ggsave("../../figures/best_article_sentiment_rolling_average_10.png", article_best, bg = "white", width = 10, height = 6)
 ggsave("../../figures/best_podcast_sentiment_rolling_average_10.png", podcast_best, bg = "white", width = 10, height = 6)
+
+# Gets the 8 emotions the podcasts and articles will be ranked by
+podcasts_nrc_emotions <- prop.table(podcasts_nrc_data[, 1:8]) %>%
+  colSums() %>%
+  data.frame(Emotion = names(.), Percentage = .)
+articles_nrc_emotions <- prop.table(articles_nrc_data[, 1:8]) %>%
+  colSums() %>%
+  data.frame(Emotion = names(.), Percentage = .)
+
+# Joins the podcast and article data
+podcast_articles_nrc_emotion_levels <- podcasts_nrc_emotions %>%
+  inner_join(articles_nrc_emotions, by = "Emotion" ) %>%
+  mutate("Podcast Percentage" = Percentage.x, "Article Percentage" = Percentage.y) %>%
+  select(Emotion,"Podcast Percentage", "Article Percentage") %>%
+  pivot_longer(cols = c("Article Percentage", "Podcast Percentage"),
+               names_to = "Media",
+               values_to = "Percentage")
+
+# Plots the 
+nrc_emotional_levels_viz <- ggplot(podcast_articles_nrc_emotion_levels, aes(x = Emotion, y = Percentage, fill = Media)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Article Emotion levels vs Podcast Emotion levels",
+       x = "Emotion", y = "Percentage of Sentiment", fill = "Media", caption = "Emotion levels from NRC Database") +
+  theme_minimal() +
+  scale_fill_manual(values = c("Article Percentage" = "steelblue", "Podcast Percentage" = "darkorange"))
 
 save.image(file = "../../data/visualizations_worspace.RData")
